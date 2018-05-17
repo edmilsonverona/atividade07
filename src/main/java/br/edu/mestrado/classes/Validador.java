15f40eb748767c2937d4a27d116bfe0303ba8c24
Edmilson <ed_verona18@hotmail.com>
2018-05-17 13:53:58 -0300

    iv - teste validação

---
+++ src/main/java/br/edu/mestrado/classes/Validador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.mestrado.classes;
+
+/**
+ *
+ * @author ed_ve
+ */
+public class Validador {
+
+    public boolean validar(double[] numeros) {
+        return true;
+    }
+    
+}
---
+++ src/test/java/br/edu/mestrado/classes/ValidadorTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.mestrado.classes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ed_ve
+ */
+public class ValidadorTest {
+    
+    public ValidadorTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void validarVetorNulo() {
+        Validador validador = new Validador();
+        assertTrue(validador.validar(null));
+    }
+}
